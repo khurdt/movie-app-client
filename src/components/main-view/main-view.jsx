5be7ac97c1dc7488b1593991df77c58d73756b8a
khurdt <joshuahurdt@gmail.com>
2022-03-03 12:00:23 -0600

    attempted adding routes, work in progress

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import axios from 'axios';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { LoginView } from '../login-view/login-view';
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
@@ -88,48 +89,64 @@ export class MainView extends React.Component {
     if (movies.length <= 2) return <div className='main-view' />;
 
     return (
-      <>
-        <Navbar style={{ backgroundColor: '#1B1D24' }} expand="lg">
+      // <Router>
+      //   <Row className="main-view justify-content-md-center">
+      //     <Route exact path="/" render={() => {
+      //       return movies.map(m => (
+      //         <Col md={3} key={m._id}>
+      //           <MovieCard movie={m} />
+      //         </Col>
+      //       ))
+      //     }} />
+      //     <Route path="/movies/:movieId" render={({ match }) => {
+      //       return <Col md={8}>
+      //         <MovieView movie={movies.find(m => m._id === match.params.movieId)} />
+      //       </Col>
+      //     }} />
+
+      //   </Row>
+      // </Router>
+      <BrowserRouter>
+        <Navbar style={{ backgroundColor: '#1E2127' }} expand='lg'>
           <Container>
-            <Navbar.Brand style={{ color: '#1266F1', fontSize: '30px' }} href="movies">myFlix</Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-              <Nav className="me-auto">
-                <Nav.Link style={{ color: 'white' }} onClick={() => { this.getMovies(token); }}>Home</Nav.Link>
+            <Navbar.Brand style={{ color: '#1266F1', fontSize: '30px' }} href='movies'>myFlix</Navbar.Brand>
+            <Navbar.Toggle aria-controls='basic-navbar-nav' />
+            <Navbar.Collapse id='basic-navbar-nav'>
+              <Nav className='me-auto'>
+                <Nav.Link style={{ color: 'white' }} onClick={() => { this.getMovies(token); }}>Movies</Nav.Link>
               </Nav>
-              <Nav className="ml-auto">
+              <Nav className='ml-auto'>
                 <Nav.Link style={{ color: 'white' }} onClick={() => { this.onLoggedOut(); }}>Logout</Nav.Link>
               </Nav>
             </Navbar.Collapse>
           </Container>
         </Navbar>
         <Container fluid style={{ width: '100%', height: 'max-content', backgroundColor: '#1B1D24' }}>
-          <div className='main-view'>
-            {/*If the state of `selectedMovie` is not null, that selected movie will be returned otherwise, all *movies will be returned*/}
-            {selectedMovie ? (//this is the same as the if statement
-              <Row className='justify-content-md-center'>
-                <Col md={8} >
-                  <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }} />
-                </Col>
-              </Row>
-            )
-              : (//this means else
-                <Row className='justify-content-md-center'>
-                  {movies.map(movie => (
-                    <Col md={4}>
-                      <MovieCard key={movie._id} movie={movie} //returns data of movie to Movie Card // id helps React better distinguish between similar elements in list
-                        //passing this eventlistener to MovieCard
-                        onMovieClick={(movie) => { this.setSelectedMovie(movie) }} />
+          <Routes>
+            <Row className='justify-content-md-center'>
+              <Route exact path='/' render={() => {
+                return (
+                  movies.map(m => (
+                    <Col md={4} key={m._id}>
+                      <MovieCard movie={m} />
                     </Col>
                   ))
-                  }
-                </Row>
-              )
-            }
-          </div>
+                );
+              }} />
+            </Row>
+            <Row className='justify-content-md-center'>
+              <Route exact path='/movies/:movieId' render={({ match }) => {
+                return (
+                  <Col md={8}>
+                    <MovieView movie={movies.find(m => m._id === match.params.movieId)} />
+                  </Col>
+                );
+              }} />
+            </Row>
+          </Routes>
         </Container>
-      </>
-      //event listener selects movie from map and resets state and pushes selected movie to movie view
+        event listener selects movie from map and resets state and pushes selected movie to movie view
+      </BrowserRouter>
     );
   }
 }
